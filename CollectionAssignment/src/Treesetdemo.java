00c127eea81267d82d40ce3e9cbd9be04772f009
Keerthanasara <kbommasa@inlen8520023442.corp.capgemini.com>
2022-11-05 08:04:19 +0530

    CollectionAssignments

---
+++ CollectionAssignment/src/Treesetdemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Treesetdemo {
+public static void main(String[] args) {
+	TreeSet<String> tree=new TreeSet<String>();
+	tree.add("keerthana");
+	tree.add("devi");
+	tree.add("sara");
+	tree.add("dora");
+	tree.add("mahes");
+	tree.add("nandhu");
+	tree.add("nandhu");
+System.out.println(tree);
+
+}
+}
