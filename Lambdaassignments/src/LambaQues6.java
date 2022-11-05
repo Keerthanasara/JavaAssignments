281324971b47ff16c2aa1a4c59700ab7bc7f23e6
Keerthanasara <kbommasa@inlen8520023442.corp.capgemini.com>
2022-11-05 21:32:18 +0530

    Lambda Assignments

---
+++ Lambdaassignments/src/LambaQues6.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+public class LambaQues6 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("alpha", "bravo", "charlie", "delta", "echo", "foxtrot");
+        LambaQues6 Letter = new LambaQues6();
+        System.out.println(Letter.convertToUpperCase(list));
+    }
+    public List<String> convertToUpperCase(List<String> list) {
+        List<String> ToUpper = new ArrayList<>();
+        Consumer<String> consumer = (str)->ToUpper.add(str.toUpperCase());
+        list.stream()
+                .forEach(consumer);
+        return  ToUpper;
+    }
+}
